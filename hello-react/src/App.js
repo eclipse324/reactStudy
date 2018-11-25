e912f0b628e23d657b2b51bd3c17399a91f95abb
eclipse324 <eclipse324@nate.com>
2018-11-25 13:54:33 +0900

    폴더정리

---
+++ hello-react/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component} from 'react';
+// import MyComponent from './MyComponent';
+// import EventPractice from './EventPractice';
+// import ValidationSample from './ValidationSample';
+// import ScrollBox from './ScrollBox';
+import IterationSample from './IterationSample';
+
+class App extends Component {
+  render() {
+    return (
+//      <MyComponent name="React" age={4} />
+//      <EventPractice />
+//      <ValidationSample />
+//      <ScrollBox />
+//      <div>
+//        <ScrollBox ref={(ref) => this.scrollBox = ref} />
+//        <button onClick={() => this.scrollBox.scrollToBottom()}> 맨 밑으로 </button>
+//      </div>
+        <IterationSample />
+    );
+  }
+}
+
+export default App;
